9dcdef8c45db197ed25e14b9bc7116d912e8d6c8
Carlos Levir <carloslevirfs@gmail.com>
2021-04-02 23:45:19 -0300

    chore: setup navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import 'react-native-gesture-handler';
+
+import Routes from './src/routes';
 
 export default function App() {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
 }
